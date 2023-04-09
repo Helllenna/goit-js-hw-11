454197ca34b853c2206b33be5ab5cbc4a42b3bfa
Helllenna <olena.penkova@gmail.com>
2023-04-09 19:30:25 +0300

    3-1

--- src/index.js
+++ src/index.js
@@ -20,12 +20,14 @@ const per_page = 40;
 let searchQuery = "";
 let startpage = 1;
 let images = '';
+let page;
 const API_KEY = "35107716-da32e5ff358c128d5e6c68865";
 const URL = "https://pixabay.com/api/";
 
 async function request(searchQuery) {
+  page = startpage;
   const urlOptions = "image_type=photo&orientation=horizontal&safesearch=true";
-  const url = `${URL}?key=${API_KEY}&q=${searchQuery}&${urlOptions}&page=${startpage}&per_page=${per_page}`;
+  const url = `${URL}?key=${API_KEY}&q=${searchQuery}&${urlOptions}&page=${page}&per_page=${per_page}`;
   const answer = await axios.get(url);
   return answer;
 }
